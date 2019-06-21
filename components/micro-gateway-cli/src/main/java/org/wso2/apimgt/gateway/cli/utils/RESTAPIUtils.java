e8fa2fe9ccbd5acec1a047023adedb32f3928b5c
Praminda Jayawardana <pramindabj@gmail.com>
2019-06-21 15:15:24 +0530

    cli: Rename misleading class
    This class is not used for anything related to token but to
    communicate with import REST APIs.

--- components/micro-gateway-cli/src/main/java/org/wso2/apimgt/gateway/cli/utils/RESTAPIUtils.java
+++ components/micro-gateway-cli/src/main/java/org/wso2/apimgt/gateway/cli/utils/RESTAPIUtils.java
@@ -23,7 +23,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class TokenManagementUtil {
+public class RESTAPIUtils {
 
     /**
      * Get input string as string
