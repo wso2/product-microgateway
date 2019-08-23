9aa37eddca472d78c58f979901c46c3ae69931e4
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2019-08-23 22:36:17 +0530

    Folder structure changes to execute at init cmd phase

--- components/micro-gateway-cli/src/main/java/org/wso2/apimgt/gateway/cli/constants/GatewayCliConstants.java
+++ components/micro-gateway-cli/src/main/java/org/wso2/apimgt/gateway/cli/constants/GatewayCliConstants.java
@@ -89,4 +89,9 @@ public class GatewayCliConstants {
 
     public static final String API_SWAGGER = "swagger.json";
     public static final String API_OPENAPI_YAML = "openAPI.yaml";
+
+    public static final String PROJECT_GRPC_DEFINITIONS_DIR = "grpc_definitions";
+    public static final String PROJECT_GRPC_OPTIONS_DIR = "gen";
+    public static final String PROJECT_GRPC_OPTIONS_FILE = "wso2CustomOptions.proto";
+    public static final String RESOURCES_GRPC_DIR = "grpc";
 }
