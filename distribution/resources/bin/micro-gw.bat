095293c08eaa2d73435162c0f4a27a81fc4ccdd7
Praminda Jayawardana <pramindabj@gmail.com>
2019-10-03 15:48:23 +0530

    cli: toolkit: Fix indentation of bat

--- distribution/resources/bin/micro-gw.bat
+++ distribution/resources/bin/micro-gw.bat
@@ -39,9 +39,9 @@ SET CURRENT_D=%CD%
 
 REM If the current disk drive ie: `E:\` is different from the drive where this (micro-gw.bat) resides(i:e `C:\`), Change the driver label the current drive
 :switchDrive
-	SET curDrive=%CURRENT_D:~0,1%
-	SET wsasDrive=%PRGDIR:~0,1%
-	if NOT "%curDrive%" == "%wsasDrive%" %wsasDrive%:
+    SET curDrive=%CURRENT_D:~0,1%
+    SET wsasDrive=%PRGDIR:~0,1%
+    if NOT "%curDrive%" == "%wsasDrive%" %wsasDrive%:
 
 REM if MICROGW_HOME environment variable not set then set it
 if "%MICROGW_HOME%" == "" SET MICROGW_HOME=%PRGDIR%..
@@ -54,45 +54,45 @@ SET PATH=%BALLERINA_HOME%\bin\;%PATH%
 
 REM Check JAVA availability
 :checkJavaHome
-	if "%JAVA_HOME%" == "" goto noJavaHome
-	if NOT EXIST "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+    if "%JAVA_HOME%" == "" goto noJavaHome
+    if NOT EXIST "%JAVA_HOME%\bin\java.exe" goto noJavaHome
 goto checkJava
 
 :noJavaHome
-	ECHO You must set the JAVA_HOME variable before running Micro-Gateway Tooling.
+    ECHO You must set the JAVA_HOME variable before running Micro-Gateway Tooling.
 goto end
 
 :checkJava
-	"%JAVA_HOME%\bin\java" -version >nul 2>&1
-	IF ERRORLEVEL 1 goto noJava
+    "%JAVA_HOME%\bin\java" -version >nul 2>&1
+    IF ERRORLEVEL 1 goto noJava
 goto runServer
 
 :noJava
-	ECHO Error: JAVA_HOME is not defined correctly.
+    ECHO Error: JAVA_HOME is not defined correctly.
 goto end
 
 :runServer
-	SET originalArgs=%*
+    SET originalArgs=%*
 
 REM Slurp the command line arguments. This loop allows for an unlimited number
 REM of arguments (up to the command line limit, anyway).
 :setupArgs
-	if ""%1""=="""" goto passToJar
-	if ""%1""==""help""     goto passToJar
-	if ""%1""==""build""     goto commandBuild
-	if ""%1""==""--java.debug""  goto commandDebug
-	SHIFT
+    if ""%1""=="""" goto passToJar
+    if ""%1""==""help""     goto passToJar
+    if ""%1""==""build""     goto commandBuild
+    if ""%1""==""--java.debug""  goto commandDebug
+    SHIFT
 goto setupArgs
 
 :commandBuild
-	REM Immediate next parameter should be project name after the `build` command
-	SHIFT
-	SET project_name=%1
-	if [%project_name%] == [] ( goto :passToJar ) else ( goto :nameFound )
+    REM Immediate next parameter should be project name after the `build` command
+    SHIFT
+    SET project_name=%1
+    if [%project_name%] == [] ( goto :passToJar ) else ( goto :nameFound )
 
-	:nameFound
-		REM Set micro gateway project directory relative to CD (current directory)
-		SET MICRO_GW_PROJECT_DIR=%CURRENT_D%\%project_name:\=%
+    :nameFound
+        REM Set micro gateway project directory relative to CD (current directory)
+        SET MICRO_GW_PROJECT_DIR=%CURRENT_D%\%project_name:\=%
 
         call :passToJar
         if ERRORLEVEL 1 (EXIT /B %ERRORLEVEL%)
@@ -101,85 +101,89 @@ goto setupArgs
         REM Set ballerina home again as the platform is extracted at this point.
         SET BALLERINA_HOME=%MICROGW_HOME%\lib\platform
         SET PATH=%BALLERINA_HOME%\bin\;%PATH%
+
         PUSHD "%CURRENT_D%"
         PUSHD "%MICRO_GW_PROJECT_DIR%\target\gen"
-            SET TARGET_DIR=%MICRO_GW_PROJECT_DIR%\target
-            SET TARGET_FILE=%TARGET_DIR%\%project_name%.balx
-            SET BUILD_STATUS=F
-            if EXIST "%TARGET_DIR%\*.balx"  DEL /F "%TARGET_DIR%\*.balx"
 
-            REM Build project using ballerina
-            call ballerina build src -o %TARGET_DIR%\%project_name:\=%.balx --offline --experimental --siddhiruntime
+        SET TARGET_DIR=%MICRO_GW_PROJECT_DIR%\target
+        SET TARGET_FILE=%TARGET_DIR%\%project_name%.balx
+        SET BUILD_STATUS=F
 
-            if ERRORLEVEL 0 (
-                if EXIST "%TARGET_FILE%" (
-                    SET BUILD_STATUS=T
-                )
+        REM Remove old build artifacts
+        if EXIST "%TARGET_DIR%\*.balx"  DEL /F "%TARGET_DIR%\*.balx"
+
+        REM Build project using ballerina
+        call ballerina build src -o %TARGET_DIR%\%project_name:\=%.balx --offline --experimental --siddhiruntime
+
+        if ERRORLEVEL 0 (
+            if EXIST "%TARGET_FILE%" (
+                SET BUILD_STATUS=T
             )
+        )
 
-            ECHO.
-            if %BUILD_STATUS%==T (
-                ECHO BUILD SUCCESSFUL
-                ECHO Target: %TARGET_FILE%
-            ) else (
-                ECHO BUILD FAILED
-            )
+        ECHO.
+        if %BUILD_STATUS%==T (
+            ECHO BUILD SUCCESSFUL
+            ECHO Target: %TARGET_FILE%
+        ) else (
+            ECHO BUILD FAILED
+        )
         POPD
 goto :end
 
 :commandDebug
-	SHIFT
-	SET DEBUG_PORT=%1
-	if "%DEBUG_PORT%"=="" goto noDebugPort
-	if NOT "%JAVA_OPTS%"=="" ECHO Warning !!!. User specified JAVA_OPTS will be ignored, once you give the --java.debug option.
-	SET JAVA_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%DEBUG_PORT%
-	ECHO Please start the remote debugging client to continue...
+    SHIFT
+    SET DEBUG_PORT=%1
+    if "%DEBUG_PORT%"=="" goto noDebugPort
+    if NOT "%JAVA_OPTS%"=="" ECHO Warning !!!. User specified JAVA_OPTS will be ignored, once you give the --java.debug option.
+    SET JAVA_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%DEBUG_PORT%
+    ECHO Please start the remote debugging client to continue...
 goto passToJar
 
 :noDebugPort
-	ECHO Please specify the debug port after the --java.debug option
+    ECHO Please specify the debug port after the --java.debug option
 goto end
 
 :passToJar
-	REM ---------- Add jars to classpath ----------------
-	SET CLI_CLASSPATH=
-	if EXIST "%BALLERINA_HOME%"\bre\lib (
-		for %%i in ("%BALLERINA_HOME%"\bre\lib\*.jar) do (
-			SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\platform\bre\lib\%%~ni%%~xi
-		)
-	) else (
-		REM Ballerina platform is not extracted yet.
-		REM Therefore we need to set cli init jars to the classpath
-		REM Platform will be extracted during the execution of Init Command
-		for %%i IN ("%MICROGW_HOME%"\lib\gateway\platform\*.jar) do (
-			SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\gateway\platform\%%~ni%%~xi
-		)
-		for %%i IN ("%MICROGW_HOME%"\lib\gateway\cli\*.jar) do (
-			SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\gateway\cli\%%~ni%%~xi
-		)
-	)
+    REM ---------- Add jars to classpath ----------------
+    SET CLI_CLASSPATH=
+    if EXIST "%BALLERINA_HOME%"\bre\lib (
+        for %%i in ("%BALLERINA_HOME%"\bre\lib\*.jar) do (
+            SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\platform\bre\lib\%%~ni%%~xi
+        )
+    ) else (
+        REM Ballerina platform is not extracted yet.
+        REM Therefore we need to set cli init jars to the classpath
+        REM Platform will be extracted during the execution of Init Command
+        for %%i IN ("%MICROGW_HOME%"\lib\gateway\platform\*.jar) do (
+            SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\gateway\platform\%%~ni%%~xi
+        )
+        for %%i IN ("%MICROGW_HOME%"\lib\gateway\cli\*.jar) do (
+            SET CLI_CLASSPATH=!CLI_CLASSPATH!;.\lib\gateway\cli\%%~ni%%~xi
+        )
+    )
 
-	SET JAVACMD=-Xms256m -Xmx1024m ^
-		-XX:+HeapDumpOnOutOfMemoryError ^
-		-XX:HeapDumpPath="%MICROGW_HOME%\heap-dump.hprof" ^
-		%JAVA_OPTS% ^
-		-classpath %CLI_CLASSPATH% ^
-		-Djava.security.egd=file:/dev/./urandom ^
-		-Dballerina.home="%BALLERINA_HOME%" ^
-		-Djava.util.logging.config.class="org.wso2.apimgt.gateway.cli.logging.CLILogConfigReader" ^
-		-Djava.util.logging.manager="org.wso2.apimgt.gateway.cli.logging.CLILogManager" ^
-		-Dfile.encoding=UTF8 ^
-		-Dtemplates.dir.path="%MICROGW_HOME%"\resources\templates ^
-		-Dcli.home="%MICROGW_HOME%" ^
-		-Dcurrent.dir="%CD%" ^
-		-DVERBOSE_ENABLED=%verbose%
+    SET JAVACMD=-Xms256m -Xmx1024m ^
+        -XX:+HeapDumpOnOutOfMemoryError ^
+        -XX:HeapDumpPath="%MICROGW_HOME%\heap-dump.hprof" ^
+        %JAVA_OPTS% ^
+        -classpath %CLI_CLASSPATH% ^
+        -Djava.security.egd=file:/dev/./urandom ^
+        -Dballerina.home="%BALLERINA_HOME%" ^
+        -Djava.util.logging.config.class="org.wso2.apimgt.gateway.cli.logging.CLILogConfigReader" ^
+        -Djava.util.logging.manager="org.wso2.apimgt.gateway.cli.logging.CLILogManager" ^
+        -Dfile.encoding=UTF8 ^
+        -Dtemplates.dir.path="%MICROGW_HOME%"\resources\templates ^
+        -Dcli.home="%MICROGW_HOME%" ^
+        -Dcurrent.dir="%CD%" ^
+        -DVERBOSE_ENABLED=%verbose%
 
 :runJava
-	REM Jump to GW-CLI exec location when running the jar
-	CD %MICROGW_HOME%
-	"%JAVA_HOME%\bin\java" %JAVACMD% org.wso2.apimgt.gateway.cli.cmd.Main %originalArgs%
-	if ERRORLEVEL 121 goto runJava
-	if ERRORLEVEL 1 (EXIT /B %ERRORLEVEL%)
+    REM Jump to GW-CLI exec location when running the jar
+    CD %MICROGW_HOME%
+    "%JAVA_HOME%\bin\java" %JAVACMD% org.wso2.apimgt.gateway.cli.cmd.Main %originalArgs%
+    if ERRORLEVEL 121 goto runJava
+    if ERRORLEVEL 1 (EXIT /B %ERRORLEVEL%)
 :end
 goto endlocal
 
