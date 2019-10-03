4bead3f2dac3c5db6b94d854fa70cb7632dd147f
Rajith90 <rajithroshan90@gmail.com>
2019-10-03 12:51:22 +0530

    Fix #832 :introduce default subscription policy

--- tests/src/test/java/org/wso2/micro/gateway/tests/common/ResponseConstants.java
+++ tests/src/test/java/org/wso2/micro/gateway/tests/common/ResponseConstants.java
@@ -39,7 +39,7 @@ public class ResponseConstants {
             + "  \"pending\": 2," + "  \"available\": 233539," + "  \"PENDING\": 1" + "}";
     public static final String PER_API_THROTTLING_RESPONSE = "{\"fault\":{\"code\":900802," +
             " \"message\":\"Message throttled out\", \"description\":\"You have exceeded your quota\"}}";
-    public static final String PER_RESOURCE_THROTTLING_RESPONSE = "{\"fault\":{\"code\":900804, " +
+    public static final String PER_RESOURCE_THROTTLING_RESPONSE = "{\"fault\":{\"code\":900802, " +
             "\"message\":\"Message throttled out\", \"description\":\"You have exceeded your quota\"}}";
     public static final String NONEXISTING_THROTTLEPOLICY_RESPONSE = "{\"fault\":{\"code\":900809, " +
             "\"message\":\"Internal server error occured\", \"description\":\"POLICY ENFORCEMENT ERROR\"}}";
