e1bdfd3152511fd13db7a00a458ed90fff1c0177
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2020-03-12 16:43:29 +0530

    Implement test cases for basicAuth scopes and different auth header with basicAuth

--- tests/src/test/java/org/wso2/micro/gateway/tests/security/ScopesTestCase.java
+++ tests/src/test/java/org/wso2/micro/gateway/tests/security/ScopesTestCase.java
@@ -30,6 +30,7 @@ import org.wso2.micro.gateway.tests.util.HttpClientRequest;
 import org.wso2.micro.gateway.tests.util.TestConstant;
 import org.wso2.micro.gateway.tests.util.TokenUtil;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +43,10 @@ public class ScopesTestCase extends BaseTestCase {
     private String jwtTokenProdWithScopes;
     private String jwtTokenProdWithSingleScope;
     private String jwtTokenProdWithMultipleScope;
+    protected String basicAuthToken;
+    private String basicAuthTokenWithSingleScope;
+    private String basicAuthTokenWithMultipleScopes;
+    private String basicAuthTokenWithScopes;
 
     @BeforeClass
     public void start() throws Exception {
@@ -67,15 +72,92 @@ public class ScopesTestCase extends BaseTestCase {
         jwtTokenProdWithMultipleScope = TokenUtil
                 .getJwtWithCustomClaims(application, new JSONObject(), TestConstant.KEY_TYPE_PRODUCTION, 3600,
                         claimMap);
+        basicAuthToken = generateBasicAuthToken("generalUser1", "password");
+        basicAuthTokenWithSingleScope = generateBasicAuthToken("generalUser2", "password");
+        basicAuthTokenWithMultipleScopes = generateBasicAuthToken("generalUser3", "password");
+        basicAuthTokenWithScopes = generateBasicAuthToken("generalUser4", "password");
+
         //generate apis with CLI and start the micro gateway server
         super.init(project, new String[] { "common_api.yaml", "security/disable_security.yaml" });
     }
 
-    @Test(description = "Test Invoking the resource which is protected by scopes without providing the scopes")
-    public void testWithoutScopes() throws Exception {
+    @Test(description = "Test Invoking the resource which is protected by scopes without providing the scopes : JWT")
+    public void testWithoutScopesJWT() throws Exception {
+        String authHeaderValue = "Bearer " + jwtTokenProd;
+        testWithoutScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource which is protected by scopes without providing the scopes : " +
+            "BasicAuth")
+    public void testWithoutScopesBasic() throws Exception {
+        String authHeaderValue = "Basic " + basicAuthToken;
+        testWithoutScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource which is protected by two scopes by providing jwt with the scope " +
+            ": JWT")
+    public void testResourceWithMultipleScopesJWT() throws Exception {
+        String authHeaderValue = "Bearer " + jwtTokenProdWithScopes;
+        testResourceWithMultipleScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource which is protected by two scopes by providing jwt with the scope " +
+            ": BasicAuth")
+    public void testResourceWithMultipleScopesBasic() throws Exception {
+        String authHeaderValue = "Basic " + basicAuthTokenWithScopes;
+        testResourceWithMultipleScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource by providing wrong scope than the expected scope : JWT")
+    public void testResourceWithWrongScopesJWT() throws Exception {
+        String authHeaderValue = "Bearer " + jwtTokenProdWithScopes;
+        testResourceWithWrongScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource by providing wrong scope than the expected scope : JWT")
+    public void testResourceWithWrongScopesBasic() throws Exception {
+        String authHeaderValue = "Basic " + basicAuthTokenWithScopes;
+        testResourceWithWrongScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource which is protected by single scope by providing jwt with the " +
+            "scope : JWT")
+    public void testResourceWithSingleScopesJWT() throws Exception {
+        String authHeaderValue = "Bearer " + jwtTokenProdWithSingleScope;
+        testResourceWithSingleScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resource which is protected by single scope by providing jwt with the " +
+            "scope : Basic")
+    public void testResourceWithSingleScopesBasic() throws Exception {
+        String authHeaderValue = "Basic " + basicAuthTokenWithSingleScope;
+        testResourceWithSingleScopes(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resources which have different scopes with same JWT having all the scopes" +
+            " : JWT")
+    public void testSameTokenWithMultipleResourcesJWT() throws Exception {
+        String authHeaderValue = "Bearer " + jwtTokenProdWithMultipleScope;
+        testSameTokenWithMultipleResources(authHeaderValue);
+    }
+
+    @Test(description = "Test Invoking the resources which have different scopes with same JWT having all the scopes" +
+            " : BASIC")
+    public void testSameTokenWithMultipleResourcesBasic() throws Exception {
+        String authHeaderValue = "Basic " + basicAuthTokenWithMultipleScopes;
+        testSameTokenWithMultipleResources(authHeaderValue);
+    }
+
+    private String generateBasicAuthToken(String username, String password) {
+        String originalInput = username + ":" + password;
+        String basicAuthToken = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        return basicAuthToken;
+    }
+
+    private void testWithoutScopes(String authHeaderValue) throws Exception {
         Map<String, String> headers = new HashMap<>();
         //test endpoint with token
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProd);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), authHeaderValue);
         // require scope write:pets or read:pets
         org.wso2.micro.gateway.tests.util.HttpResponse response = HttpClientRequest
                 .doPost(getServiceURLHttp("petstore/v1/pet/1"), "{}", headers);
@@ -85,11 +167,10 @@ public class ScopesTestCase extends BaseTestCase {
         Assert.assertEquals(response.getResponseCode(), 403, "HTTP Response code mismatched");
     }
 
-    @Test(description = "Test Invoking the resource which is protected by two scopes by providing jwt with the scope")
-    public void testResourceWithMultipleScopes() throws Exception {
+    private void testResourceWithMultipleScopes(String authHeaderValue) throws Exception {
         Map<String, String> headers = new HashMap<>();
         //test endpoint with token
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProdWithScopes);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), authHeaderValue);
         // require scope write:pets or read:pets
         org.wso2.micro.gateway.tests.util.HttpResponse response = HttpClientRequest
                 .doPost(getServiceURLHttp("petstore/v1/pet/1"), "{}", headers);
@@ -98,11 +179,10 @@ public class ScopesTestCase extends BaseTestCase {
         Assert.assertEquals(response.getResponseCode(), 200, "Response code mismatched");
     }
 
-    @Test(description = "Test Invoking the resource by providing wrong scope than the expected scope")
-    public void testResourceWithWrongScopes() throws Exception {
+    private void testResourceWithWrongScopes(String authHeaderValue) throws Exception {
         Map<String, String> headers = new HashMap<>();
         //test endpoint with token
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProdWithScopes);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), authHeaderValue);
         // require scope write:petsNew
         org.wso2.micro.gateway.tests.util.HttpResponse response = HttpClientRequest
                 .doPost(getServiceURLHttp("petstore/v1/pet"), "{}", headers);
@@ -112,11 +192,10 @@ public class ScopesTestCase extends BaseTestCase {
         Assert.assertEquals(response.getResponseCode(), 403, "HTTP Response code mismatched");
     }
 
-    @Test(description = "Test Invoking the resource which is protected by single scope by providing jwt with the scope")
-    public void testResourceWithSingleScopes() throws Exception {
+    private void testResourceWithSingleScopes(String authHeaderValue) throws Exception {
         Map<String, String> headers = new HashMap<>();
         //test endpoint with token
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProdWithSingleScope);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), authHeaderValue);
         // require scope write:petsNew
         org.wso2.micro.gateway.tests.util.HttpResponse response = HttpClientRequest
                 .doPost(getServiceURLHttp("petstore/v1/pet/"), "{}", headers);
@@ -125,11 +204,10 @@ public class ScopesTestCase extends BaseTestCase {
         Assert.assertEquals(response.getResponseCode(), 200, "Response code mismatched");
     }
 
-    @Test(description = "Test Invoking the resources which have different scopes with same JWT having all the scopes")
-    public void testSameTokenWithMultipleResources() throws Exception {
+    private void testSameTokenWithMultipleResources(String authHeaderValue) throws Exception {
         Map<String, String> headers = new HashMap<>();
         //test endpoint with token
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProdWithMultipleScope);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), authHeaderValue);
         // require scope write:pets or read:pets
         org.wso2.micro.gateway.tests.util.HttpResponse response = HttpClientRequest
                 .doPost(getServiceURLHttp("petstore/v1/pet/1"), "{}", headers);
@@ -143,5 +221,4 @@ public class ScopesTestCase extends BaseTestCase {
         Assert.assertEquals(response.getData(), ResponseConstants.petByIdResponse);
         Assert.assertEquals(response.getResponseCode(), 200, "Response code mismatched");
     }
-
 }
