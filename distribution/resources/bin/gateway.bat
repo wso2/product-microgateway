4a794bb78fdad82f1acb5008c10b9f698b7e0540
Praminda Jayawardana <pramindabj@gmail.com>
2020-03-13 12:33:22 +0530

    runtime: cli: Fix error log in startup
    Fixes #1107
    
    No need to pass the command name to tools lib now. It only contain
    one command at the moment.
    CONF_OUT_FILE is not written to disk if observability configuration
    is not found on the micro-gw.conf file. Therefore error log should
    not be printed if CONF_OUT_FILE is not found

--- distribution/resources/bin/gateway.bat
+++ distribution/resources/bin/gateway.bat
@@ -70,7 +70,7 @@ IF %ERRORLEVEL% NEQ 0 GOTO END
 SET EXEC_FILE=%~1
 CALL :buildBalArgs %*
 
-CALL :runTools "getConfig %CONF_FILE% %CONF_OUT_FILE%"
+CALL :runTools "%CONF_FILE% %CONF_OUT_FILE%"
 
 IF "%b7a_observability_metrics_enabled%"=="true" (
     SET IS_METRICS_ENABLED=T
@@ -86,9 +86,6 @@ IF EXIST %CONF_OUT_FILE% (
             GOTO :enableAgent
         )
     )
-) ELSE (
-    ECHO Error while reading observability configurations from the file
-    GOTO :continueInit
 )
 
 :enableAgent
@@ -134,7 +131,7 @@ REM Start the gateway using internal ballerina distribution as the runtime
     IF %ERRORLEVEL% NEQ 0 (
         ECHO WARN: Can't find powershell in the system!
         ECHO WARN: STDERR and STDOUT will be piped to %GW_HOME%\logs\microgateway.log
-        "%JAVA_HOME%\bin\java.exe" %JAVA_ARGS% -Dmgw-runtime.home=%GW_HOME% -Dballerina.home=%GW_HOME%/runtime -jar "%EXEC_FILE%" %BAL_ARGS% --api.usage.data.path=%USAGE_DATA_PATH% --b7a.config.file="%GW_HOME%\conf\micro-gw.conf" >> "%GW_HOME%\logs\microgateway.log" 2>&1
+        "%JAVA_HOME%\bin\java.exe" %JAVA_ARGS% -Dmgw-runtime.home=%GW_HOME% -Dballerina.home=%GW_HOME%/runtime -Djava.util.logging.config.class=org.ballerinalang.logging.util.LogConfigReader -Djava.util.logging.manager=org.ballerinalang.logging.BLogManager -jar "%EXEC_FILE%" %BAL_ARGS% --api.usage.data.path=%USAGE_DATA_PATH% --b7a.config.file="%GW_HOME%\conf\micro-gw.conf" >> "%GW_HOME%\logs\microgateway.log" 2>&1
 
         EXIT /B %ERRORLEVEL%
     ) ELSE (
@@ -151,11 +148,11 @@ REM Start the gateway using internal ballerina distribution as the runtime
 
         REM TODO: Possible solution for this complexity can be Add-Content Cmdlet. Do some RnD on it.
         IF !PSVersion! LEQ 3 (
-            CALL POWERSHELL "%JAVA_HOME%\bin\java.exe %JAVA_ARGS% '-Dmgw-runtime.home=%GW_HOME%' '-Dballerina.home=%GW_HOME%/runtime' -Djava.util.logging.config.class="org.ballerinalang.logging.util.LogConfigReader" -Djava.util.logging.manager="org.ballerinalang.logging.BLogManager" -jar '%EXEC_FILE%' %BAL_ARGS% --api.usage.data.path='%USAGE_DATA_PATH%' --b7a.config.file='%GW_HOME%\conf\micro-gw.conf' | out-file -encoding Unicode -filepath '%GW_HOME%\logs\microgateway.log' -Append"
+            CALL POWERSHELL "%JAVA_HOME%\bin\java.exe %JAVA_ARGS% '-Dmgw-runtime.home=%GW_HOME%' '-Dballerina.home=%GW_HOME%/runtime' '-Djava.util.logging.config.class=org.ballerinalang.logging.util.LogConfigReader' '-Djava.util.logging.manager=org.ballerinalang.logging.BLogManager' -jar '%EXEC_FILE%' %BAL_ARGS% --api.usage.data.path='%USAGE_DATA_PATH%' --b7a.config.file='%GW_HOME%\conf\micro-gw.conf' | out-file -encoding Unicode -filepath '%GW_HOME%\logs\microgateway.log' -Append"
             EXIT /B %ERRORLEVEL%
         ) ELSE (
             REM For powershell version 4 or above , We can use `tee` command for output to both file stream and stdout (Ref: https://en.wikipedia.org/wiki/PowerShell#PowerShell_4.0)
-            CALL POWERSHELL "!JAVA_HOME!\bin\java.exe %JAVA_ARGS% '-Dmgw-runtime.home=%GW_HOME%' '-Dballerina.home=%GW_HOME%/runtime' -Djava.util.logging.config.class="org.ballerinalang.logging.util.LogConfigReader" -Djava.util.logging.manager="org.ballerinalang.logging.BLogManager" -jar '%EXEC_FILE%' %BAL_ARGS% --api.usage.data.path='%USAGE_DATA_PATH%' --b7a.config.file='%GW_HOME%\conf\micro-gw.conf' | tee -Append %GW_HOME%\logs\microgateway.log"
+            CALL POWERSHELL "!JAVA_HOME!\bin\java.exe %JAVA_ARGS% '-Dmgw-runtime.home=%GW_HOME%' '-Dballerina.home=%GW_HOME%/runtime' '-Djava.util.logging.config.class=org.ballerinalang.logging.util.LogConfigReade' '-Djava.util.logging.manager=org.ballerinalang.logging.BLogManager' -jar '%EXEC_FILE%' %BAL_ARGS% --api.usage.data.path='%USAGE_DATA_PATH%' --b7a.config.file='%GW_HOME%\conf\micro-gw.conf' | tee -Append %GW_HOME%\logs\microgateway.log"
             EXIT /B %ERRORLEVEL%
         )
     )
