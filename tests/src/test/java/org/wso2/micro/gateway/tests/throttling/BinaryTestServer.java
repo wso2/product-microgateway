207766f20d0dcf41e897b7fc72b1ad781cfed722
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2020-07-16 18:16:52 +0530

    add-licence-header

--- tests/src/test/java/org/wso2/micro/gateway/tests/throttling/BinaryTestServer.java
+++ tests/src/test/java/org/wso2/micro/gateway/tests/throttling/BinaryTestServer.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.micro.gateway.tests.throttling;
 
 import org.slf4j.Logger;
@@ -21,7 +39,6 @@ import org.wso2.carbon.databridge.receiver.binary.internal.BinaryDataReceiver;
 import java.io.IOException;
 import java.net.SocketException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Binary Test Server.
@@ -74,7 +91,6 @@ public class BinaryTestServer {
             return 100000;
         }
     };
-
 
     public void startServer(int tcpPort, int securePort) throws DataBridgeException, IOException {
         DataPublisherTestUtil.setKeyStoreParams();
