a97733f9023be5e08c4a3f949a519ad5120a201e
Praminda Jayawardana <pramindabj@gmail.com>
2020-11-12 14:32:01 +0530

    dist: Rename components

---
+++ adapter/cmd/microgateway/bootsrap.go
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package microgateway
+
+import (
+	logger "github.com/sirupsen/logrus"
+	"github.com/wso2/micro-gw/configs"
+	"github.com/wso2/micro-gw/pkg/mgw"
+)
+
+func initServer() error {
+	return nil
+}
+
+/**
+ * Start the adapter.
+ *
+ */
+func StartMicroGateway(args []string) {
+
+	logger.Info("Starting Microgateway")
+	err := initServer()
+	if err != nil {
+		logger.Fatal("Error starting the adapter", err)
+	}
+	conf, errReadConfig := configs.ReadConfigs()
+	if errReadConfig != nil {
+		logger.Fatal("Error loading configuration. ", errReadConfig)
+	}
+	mgw.Run(conf)
+}
---
+++ enforcer/src/main/java/org/wso2/micro/gateway/enforcer/security/AuthFilter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.micro.gateway.enforcer.security;
+
+import org.wso2.micro.gateway.enforcer.Filter;
+import org.wso2.micro.gateway.enforcer.api.RequestContext;
+import org.wso2.micro.gateway.enforcer.api.config.APIConfig;
+import org.wso2.micro.gateway.enforcer.exception.APISecurityException;
+import org.wso2.micro.gateway.enforcer.security.jwt.JWTAuthenticator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is the filter handling the authentication for the requests flowing through the gateway.
+ */
+public class AuthFilter implements Filter {
+    private List<Authenticator> authenticators = new ArrayList<>();
+
+    @Override
+    public void init(APIConfig apiConfig) {
+        //TODO: Check security schema and add relevant authenticators .
+        Authenticator jwtAuthenticator = new JWTAuthenticator();
+        authenticators.add(jwtAuthenticator);
+    }
+
+    @Override
+    public boolean handleRequest(RequestContext requestContext) {
+        try {
+            for (Authenticator authenticator : authenticators) {
+                if (authenticator.canAuthenticate(requestContext)) {
+                    AuthenticationContext authenticate = authenticator.authenticate(requestContext);
+                    if (authenticate.isAuthenticated()) {
+                        return true;
+                    }
+                }
+            }
+        } catch (APISecurityException e) {
+            requestContext.getProperties().put("code", e.getErrorCode());
+            requestContext.getProperties().put("error_code", e.getErrorCode());
+            requestContext.getProperties().put("error_description", e.getMessage());
+        }
+        return false;
+    }
+}
