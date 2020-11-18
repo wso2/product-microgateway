013233f80ef59c735d6a211f55bc6c81e1797154
chashikajw <chashikajw007@gmail.com>
2020-11-18 11:57:52 +0530

    setup testcontainers

---
+++ test-integration/src/test/java/org/wso2am/micro/gw/tests/IntegrationTest.java
@@ -0,0 +1,3 @@
+package org.wso2am.micro.gw.tests;
+
+public interface IntegrationTest {}
--- test-integration/src/test/java/org/wso2am/micro/gw/tests/dummy/DummyTestCase.java
+++ test-integration/src/test/java/org/wso2am/micro/gw/tests/dummy/DummyTestCase.java
@@ -1,12 +1,42 @@
 package org.wso2am.micro.gw.tests.dummy;
 
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.wso2am.micro.gw.tests.IntegrationTest;
 
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-
+@Category(IntegrationTest.class)
 public class DummyTestCase {
-    @Test(description = "dummy test case")
-    void alwaysTrue() {
-        assertTrue(true);
+
+    @ClassRule
+    public static DockerComposeContainer environment =
+            new DockerComposeContainer(new File("src/test/resources/compose-test.yml"))
+            .withExposedService("enforcer",9095);
+
+
+
+    @Before
+    public void setData() {
+        //environment.start();
+
+
+    }
+
+    @Test
+    public void testAssertEqualsFalse() {
+        //  processed the item
+        int one_ = 1;
+        int two_ = 2;
+
+        assertEquals(true,true);
     }
 }
