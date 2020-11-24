9b8ab4bb2a777afab0b5ddbbd763151c779c0255
chashikajw <chashikajw007@gmail.com>
2020-11-24 09:39:02 +0530

    implement jwt test cases

--- test-integration/src/test/java/org/wso2am/micro/gw/tests/common/BaseTestCase.java
+++ test-integration/src/test/java/org/wso2am/micro/gw/tests/common/BaseTestCase.java
@@ -1,6 +1,112 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2am.micro.gw.tests.common;
 
 
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.wso2am.micro.gw.tests.common.model.API;
+import org.wso2am.micro.gw.tests.common.model.ApplicationDTO;
+import org.wso2am.micro.gw.tests.common.model.SubscribedApiDTO;
+import org.wso2am.micro.gw.tests.context.MgwServerInstance;
+import org.wso2am.micro.gw.tests.context.MicroGWTestException;
+import org.wso2am.micro.gw.tests.util.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+
+
 public class BaseTestCase {
-    
+
+    protected MgwServerInstance microGWServer;
+
+
+    public void startMGW() throws MicroGWTestException {
+        //Utils.checkPortAvailability(TestConstant.ADAPTER_IMPORT_API_PORT);
+        //Utils.checkPortAvailability(TestConstant.GATEWAY_LISTENER_HTTPS_PORT);
+        microGWServer.startMGW();
+
+    }
+
+    public void stopMGW() {
+        microGWServer.stopMGW();
+
+    }
+
+    //utill class
+    public void deployAPI(String apiZipFilePath, String TrustStorePath) throws Exception {
+
+        // Set header
+        Map<String, String> headers = new HashMap<String,String>();
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Basic YWRtaW46YWRtaW4=");
+        HttpsPostMultipart multipart = new HttpsPostMultipart(getImportAPIServiceURLHttps(
+                "/mgw/1.0/import/api") , headers, TrustStorePath);
+        multipart.addFilePart("file", new File(apiZipFilePath));
+        HttpResponse response = multipart.getResponse();
+
+        // todo
+        TimeUnit.SECONDS.sleep(5);
+        Assert.assertNotNull(response);
+        Assert.assertEquals("Response code mismatched", HttpStatus.SC_SUCCESS, response.getResponseCode());
+    }
+
+
+
+    public static String getImportAPIServiceURLHttps(String servicePath) throws MalformedURLException {
+        return new URL(new URL("https://localhost:" + TestConstant.ADAPTER_IMPORT_API_PORT), servicePath)
+                .toString();
+    }
+
+    public static String getServiceURLHttps(String servicePath) throws MalformedURLException {
+        return new URL(new URL("https://localhost:" + TestConstant.GATEWAY_LISTENER_HTTPS_PORT), servicePath)
+                .toString();
+    }
+
+    protected String getMockServiceURLHttp(String servicePath) throws MalformedURLException {
+        return new URL(new URL("https://localhost:" + TestConstant.MOCK_SERVER_PORT), servicePath).toString();
+    }
+
+    protected String getJWT(API api, ApplicationDTO applicationDTO, String tier, String keyType, int validityPeriod)
+            throws Exception {
+        SubscribedApiDTO subscribedApiDTO = new SubscribedApiDTO();
+        subscribedApiDTO.setContext(api.getContext() + "/" + api.getVersion());
+        subscribedApiDTO.setName(api.getName());
+        subscribedApiDTO.setVersion(api.getVersion());
+        subscribedApiDTO.setPublisher("admin");
+
+        subscribedApiDTO.setSubscriptionTier(tier);
+        subscribedApiDTO.setSubscriberTenantDomain("carbon.super");
+
+        JSONObject jwtTokenInfo = new JSONObject();
+        jwtTokenInfo.put("subscribedAPIs", new JSONArray(Arrays.asList(subscribedApiDTO)));
+        return TokenUtil.getBasicJWT(applicationDTO, jwtTokenInfo, keyType, validityPeriod);
+    }
+
 }
--- test-integration/src/test/java/org/wso2am/micro/gw/tests/jwtValidator/JwtTestCase.java
+++ test-integration/src/test/java/org/wso2am/micro/gw/tests/jwtValidator/JwtTestCase.java
@@ -1,86 +1,134 @@
-package org.wso2am.micro.gw.tests.dummy;
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2am.micro.gw.tests.jwtValidator;
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import org.junit.*;
 import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.testcontainers.containers.DockerComposeContainer;
 import org.wso2am.micro.gw.tests.IntegrationTest;
 import org.wso2am.micro.gw.tests.common.BaseTestCase;
+import org.wso2am.micro.gw.tests.common.model.API;
+import org.wso2am.micro.gw.tests.common.model.ApplicationDTO;
 import org.wso2am.micro.gw.tests.util.*;
-import java.io.File;
+import org.junit.platform.runner.JUnitPlatform;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.wso2am.micro.gw.tests.common.BaseTestCase.getImportAPIServiceURLHttps;
-
 
 @Category(IntegrationTest.class)
-public class DummyTestCase extends BaseTestCase {
+public class JwtTestCase extends BaseTestCase {
 
+    protected String jwtTokenProd;
 
     @Before
     public void start() throws Exception {
-        super.startSetup();
+        super.startMGW();
 
-        String apiZipfile = "/home/chashika/Desktop/petstore.zip";
-        String certificatesTrustStorePath = "/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/security/cacerts";
+        //deploy the api
+
+        //todo create a zip file
+        String apiZipfile = getClass().getClassLoader()
+                .getResource("apis/petstore.zip").getPath();
+        String certificatesTrustStorePath = getClass().getClassLoader()
+                .getResource("keystore/cacerts").getPath();
         super.deployAPI(apiZipfile,certificatesTrustStorePath);
+
+        //generate JWT token from APIM
+        API api = new API();
+        api.setName("PetStoreAPI");
+        api.setContext("petstore/v1");
+        api.setProdEndpoint(getMockServiceURLHttp("/echo/prod"));
+        api.setVersion("1.0.0");
+        api.setProvider("admin");
+
+        //Define application info
+        ApplicationDTO application = new ApplicationDTO();
+        application.setName("jwtApp");
+        application.setTier("Unlimited");
+        application.setId((int) (Math.random() * 1000));
+
+        jwtTokenProd = getJWT(api, application, "Unlimited", TestConstant.KEY_TYPE_PRODUCTION, 3600);
+        System.out.println(jwtTokenProd );
     }
+
+
+    /*@After
+    public void stop() {
+        super.stopMGW();
+    } */
+
 
     @Test
     @DisplayName("Test to check the JWT auth working")
     public void invokeJWTHeaderSuccessTest() throws Exception{
 
-        String certificatesTrustStorePath = "/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/security/cacerts";
-        //System.setProperty("javax.net.ssl.trustStore", certificatesTrustStorePath);
+        String certificatesTrustStorePath = getClass().getClassLoader()
+                .getResource("keystore/cacerts").getPath();
 
         // Set header
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + TestConstant.VALID_JWT_TOKEN);
+        headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + jwtTokenProd);
         HttpResponse response = HttpsClientRequest.doGet(getServiceURLHttps(
-                "/v2/pet/1") , headers, certificatesTrustStorePath);
+                "/v2/pet/3") , headers, certificatesTrustStorePath);
 
         Assert.assertNotNull(response);
-        Assert.assertEquals("Response code mismatched",response.getResponseCode(), HttpStatus.SC_OK);
+        Assert.assertEquals("Response code mismatched", HttpStatus.SC_OK, response.getResponseCode());
     }
 
     @Test
-    @DisplayName("Test to check the JWT auth validate token")
+    @DisplayName("Test to check the JWT auth validate invalida signature token")
     public void invokeJWTHeaderInvalidTokenTest() throws Exception{
 
-        String certificatesTrustStorePath = "/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/security/cacerts";
+        String certificatesTrustStorePath = getClass().getClassLoader()
+                .getResource("keystore/cacerts").getPath();
 
         // Set header
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + TestConstant.INVALID_JWT_TOKEN);
         HttpResponse response = HttpsClientRequest.doGet(getServiceURLHttps(
-                "/v2/pet/1") , headers, certificatesTrustStorePath);
+                "/v2/pet/3") , headers, certificatesTrustStorePath);
 
         Assert.assertNotNull(response);
-        Assert.assertEquals("Response code mismatched",response.getResponseCode(),
-                TestConstant.INVALID_CREDENTIALS_CODE);
+        Assert.assertEquals("Response code mismatched",TestConstant.INVALID_CREDENTIALS_CODE,
+                response.getResponseCode());
     }
 
     @Test
-    @DisplayName("Test to check the JWT auth validate token")
+    @DisplayName("Test to check the JWT auth validate expired token")
     public void invokeJWTHeaderExpiredTokenTest() throws Exception{
 
-        String certificatesTrustStorePath = "/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/security/cacerts";
+        String certificatesTrustStorePath = getClass().getClassLoader()
+                .getResource("keystore/cacerts").getPath();
 
         // Set header
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(HttpHeaderNames.AUTHORIZATION.toString(), "Bearer " + TestConstant.EXPIRED_JWT_TOKEN);
         HttpResponse response = HttpsClientRequest.doGet(getServiceURLHttps(
-                "/v2/pet/1") , headers, certificatesTrustStorePath);
+                "/v2/pet/3") , headers, certificatesTrustStorePath);
 
         Assert.assertNotNull(response);
-        Assert.assertEquals("Response code mismatched",response.getResponseCode(),
-                TestConstant.INVALID_CREDENTIALS_CODE);
+        Assert.assertEquals("Response code mismatched", TestConstant.INVALID_CREDENTIALS_CODE,
+                response.getResponseCode());
     }
-
-
 
 }
