ea0fb028303ed596ba97d346872706aed1fb410e
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2021-01-06 10:57:13 +0530

    refactor file name router certificate and private key

--- adapter/pkg/oasparser/envoyconf/envoyconf_internal_test.go
+++ adapter/pkg/oasparser/envoyconf/envoyconf_internal_test.go
@@ -334,8 +334,8 @@ func TestCreateUpstreamTLSContext(t *testing.T) {
 	certFilePath := config.GetMgwHome() + "/../adapter/test-resources/envoycodegen/certs/testcrt.crt"
 	certByteArr, err := ioutil.ReadFile(certFilePath)
 	assert.Nil(t, err, "Error while reading the certificate : "+certFilePath)
-	defaultMgwKeyPath := "/home/wso2/security/localhost.key"
-	defaultMgwCertPath := "/home/wso2/security/localhost.pem"
+	defaultMgwKeyPath := "/home/wso2/security/mg.key"
+	defaultMgwCertPath := "/home/wso2/security/mg.pem"
 	defaultCipherArray := "ECDHE-ECDSA-AES128-GCM-SHA256, ECDHE-RSA-AES128-GCM-SHA256, ECDHE-ECDSA-AES128-SHA," +
 		" ECDHE-RSA-AES128-SHA, AES128-GCM-SHA256, AES128-SHA, ECDHE-ECDSA-AES256-GCM-SHA384, ECDHE-RSA-AES256-GCM-SHA384," +
 		" ECDHE-ECDSA-AES256-SHA, ECDHE-RSA-AES256-SHA, AES256-GCM-SHA384, AES256-SHA"
