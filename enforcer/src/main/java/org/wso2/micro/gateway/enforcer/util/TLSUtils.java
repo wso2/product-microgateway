80e8e748b1dd52c0ccfc3bcaf9625eecdca0bf7f
Shalki <swenushika@gmail.com>
2021-02-15 19:58:11 +0530

    Change droppings folder location

--- enforcer/src/main/java/org/wso2/micro/gateway/enforcer/util/TLSUtils.java
+++ enforcer/src/main/java/org/wso2/micro/gateway/enforcer/util/TLSUtils.java
@@ -23,7 +23,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.wso2.micro.gateway.enforcer.config.ConfigHolder;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
