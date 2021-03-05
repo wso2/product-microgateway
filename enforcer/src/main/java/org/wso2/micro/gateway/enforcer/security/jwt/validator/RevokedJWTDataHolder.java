82a6b4715a96bb5cc089f80aaaf4317c7dce72e5
dinusha92 <dinushad92@gmail.com>
2021-03-05 16:49:39 +0530

    Check if evenhub is enabled.

--- enforcer/src/main/java/org/wso2/micro/gateway/enforcer/security/jwt/validator/RevokedJWTDataHolder.java
+++ enforcer/src/main/java/org/wso2/micro/gateway/enforcer/security/jwt/validator/RevokedJWTDataHolder.java
@@ -20,6 +20,7 @@ package org.wso2.micro.gateway.enforcer.security.jwt.validator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.wso2.micro.gateway.enforcer.config.ConfigHolder;
 import org.wso2.micro.gateway.enforcer.discovery.RevokedTokenDiscoveryClient;
 
 import java.util.Map;
@@ -35,8 +36,10 @@ public class RevokedJWTDataHolder {
     private static RevokedJWTDataHolder instance = new RevokedJWTDataHolder();
 
     public void init() {
-        RevokedTokenDiscoveryClient revokedTokenDs =  RevokedTokenDiscoveryClient.getInstance();
-        revokedTokenDs.watchRevokedTokens();
+        if (ConfigHolder.getInstance().getConfig().getEventHub().isEnabled()) {
+            RevokedTokenDiscoveryClient revokedTokenDs =  RevokedTokenDiscoveryClient.getInstance();
+            revokedTokenDs.watchRevokedTokens();
+        }
     }
 
     /**
