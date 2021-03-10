f0229f31650f7dec4c1f4abb8e2176a8b5f3a0c0
Subhashinie Koshalya <subhashinie.ksw@gmail.com>
2021-03-10 16:58:05 +0530

    move all test cases to a common folder

--- test/test-integration/src/test/java/org/wso2am/micro/gw/tests/testCases/security/CorsTestCase.java
+++ test/test-integration/src/test/java/org/wso2am/micro/gw/tests/testCases/security/CorsTestCase.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2am.micro.gw.tests.security;
+package org.wso2am.micro.gw.tests.testCases.security;
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
 import io.netty.handler.codec.http.HttpHeaderNames;
