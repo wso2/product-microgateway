981fac68303aa290fb8d2d0db968be22646236ac
hiranya <hiranyakavi@gmail.com>
2021-04-02 11:16:24 +0530

    Fixing invokeRemoveAuthHeaderSuccessTest issue

--- adapter/internal/oasparser/envoyconf/routes_with_clusters.go
+++ adapter/internal/oasparser/envoyconf/routes_with_clusters.go
@@ -432,7 +432,7 @@ func createRoute(params *routeCreateParams) *routev3.Route {
 		logger.LoggerOasparser.Fatal("Error loading configuration. ", errReadConfig)
 	}
 
-	if conf.Security.Adapter.EnableOutboundAuthHeader {
+	if !conf.Security.Adapter.EnableOutboundAuthHeader {
 		var internalKey string = "Internal-Key"
 		logger.LoggerOasparser.Debugf("removeHeader: %v", authHeader)
 		if authHeader == "" {
