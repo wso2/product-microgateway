f4b17202556e21adaff1693a654ae618c0120a8e
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2021-04-23 19:38:27 +0530

    revert using jms events to populate APIList

--- adapter/internal/messaging/notification_listener.go
+++ adapter/internal/messaging/notification_listener.go
@@ -150,36 +150,12 @@ func handleAPIEvents(data []byte, eventType string) {
 								return
 							}
 						}
-						var apiEvent APIEvent
-						json.Unmarshal([]byte(string(data)), &apiEvent)
-						api := types.API{
-							APIID:    apiEvent.APIID,
-							UUID:     apiEvent.UUID,
-							Provider: apiEvent.APIProvider,
-							// hardcoded as default version is not assigned at the moment.
-							IsDefaultVersion: false,
-							APIStatus:        apiEvent.APIStatus,
-							TenantID:         apiEvent.TenantID,
-							TenantDomain:     apiEvent.TenantDomain,
-							TimeStamp:        apiEvent.TimeStamp,
-						}
-
-						// Currently the API Deploy/Remove event and the other API events use different keys
-						// Added this logic to prevent any failures if the API Manager implementation is changed.
-						if apiEvent.APIName != "" {
-							api.Name = apiEvent.APIName
-						} else {
-							api.Name = apiEvent.Name
-						}
-
-						if apiEvent.APIVersion != "" {
-							api.Version = apiEvent.APIVersion
-						} else {
-							api.Version = apiEvent.Version
-						}
-
-						eh.APIListMap[env].List = append(eh.APIListMap[env].List, api)
-						xds.UpdateEnforcerAPIList(env, xds.MarshalAPIList(eh.APIListMap[env]))
+						queryParamMap := make(map[string]string, 3)
+						queryParamMap[eh.GatewayLabelParam] = configuredEnv
+						queryParamMap[eh.ContextParam] = apiEvent.Context
+						queryParamMap[eh.VersionParam] = apiEvent.Version
+						go eh.InvokeService(eh.ApisEndpoint, eh.APIListMap[env], queryParamMap,
+							eh.APIListChannel, 0)
 					}
 				}
 			}
