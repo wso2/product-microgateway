4cf3fe26f2a9dd576277716e6e840717938c901a
amali <maamalilakshika@gmail.com>
2021-07-19 10:18:20 +0530

    Send deployment status

---
+++ adapter/internal/notifier/deployment_notifier.go
@@ -0,0 +1,74 @@
+package notifier
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/wso2/product-microgateway/adapter/config"
+	"github.com/wso2/product-microgateway/adapter/internal/auth"
+	logger "github.com/wso2/product-microgateway/adapter/internal/loggers"
+	"github.com/wso2/product-microgateway/adapter/pkg/tlsutils"
+	"net/http"
+	"strings"
+)
+
+const (
+	deployedRevisionEP  string = "internal/data/v1/apis/deployed-revision"
+)
+//UpdateDeployedRevisions create the DeployedAPIRevision object
+func UpdateDeployedRevisions(apiID string, revisionID int, envs []string, vhost string) *DeployedAPIRevision {
+	revisions := &DeployedAPIRevision{
+		APIID:    apiID,
+		RevisionID: revisionID,
+		EnvInfo:    []DeployedEnvInfo{},
+	}
+	for _, env := range envs {
+		info := DeployedEnvInfo{
+			Name:  env,
+			VHost: vhost,
+		}
+		revisions.EnvInfo = append(revisions.EnvInfo, info)
+	}
+	return revisions
+}
+
+//SendRevisionUpdate sends deployment status to the control plane
+func SendRevisionUpdate(deployedRevisionList []*DeployedAPIRevision) {
+	logger.LoggerNotifier.Debugf("Revision deployed message is sending to Control plane")
+	conf, _ := config.ReadConfigs()
+	cpConfigs := conf.ControlPlane
+
+	revisionEP := cpConfigs.ServiceURL
+	if strings.HasSuffix(revisionEP, "/") {
+		revisionEP += deployedRevisionEP
+	} else {
+		revisionEP += "/" + deployedRevisionEP
+	}
+
+	if len(deployedRevisionList) < 1 || !cpConfigs.Enabled {
+		return
+	}
+
+	jsonValue, _ := json.Marshal(deployedRevisionList)
+	req, _ := http.NewRequest("POST", revisionEP, bytes.NewBuffer(jsonValue))
+	// Setting authorization header
+	basicAuth := "Basic " + auth.GetBasicAuth(cpConfigs.Username, cpConfigs.Password)
+	req.Header.Set("Authorization", basicAuth)
+	req.Header.Set("Content-Type", "application/json")
+
+	// Adding 3 retries for revision update sending
+	retries := 0
+	for retries < 3 {
+		retries++
+		resp, err := tlsutils.InvokeControlPlane(req, cpConfigs.SkipSSLVerification)
+		if err != nil {
+			logger.LoggerNotifier.Warnf("Error response from %v for retry attempt %v : %v", revisionEP, retries, err.Error())
+			continue
+		}
+		if resp.StatusCode != http.StatusCreated {
+			logger.LoggerNotifier.Warnf("Error response status code %v from %v for retry attempt %v", resp.StatusCode, revisionEP, retries)
+			continue
+		}
+		logger.LoggerNotifier.Debugf("Revision deployed message sent to Control plane: %v", string(jsonValue))
+		break
+	}
+}
