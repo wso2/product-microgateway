6b9b7bddfc28a166f04809a367622a18e25b676d
dnwick <dnsnipe123@gmail.com>
2021-07-27 16:15:33 +0530

    Fix license headers

--- adapter/pkg/messaging/azure_connection.go
+++ adapter/pkg/messaging/azure_connection.go
@@ -1,20 +1,17 @@
 /*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *  *
- *  * WSO2 Inc. licenses this file to you under the Apache License,
- *  * Version 2.0 (the "License"); you may not use this file except
- *  * in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *     http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing,
- *  * software distributed under the License is distributed on an
- *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  * KIND, either express or implied. See the License for the
- *  * specific language governing permissions and limitations
- *  * under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
 
@@ -34,21 +31,16 @@ func init() {
 	AzureRevokedTokenChannel = make(chan []byte)
 }
 
-
 // connectToAzureServiceBus function tries to connect to the azure service bus server as long as it takes to
 // establish a connection
 func connectToAzureServiceBus() {
 	logger.LoggerMgw.Info("[TEST][FEATURE_FLAG_REPLACE_EVENT_HUB] Trying to connect to azure service bus")
 }
 
-
-
 // InitiateBrokerConnection to pass event consumption
 func InitiateBrokerConnection(eventListeningEndpoints []string) error {
 	var err error
 	connectToAzureServiceBus()
-
 	err = nil
-
 	return err
 }
