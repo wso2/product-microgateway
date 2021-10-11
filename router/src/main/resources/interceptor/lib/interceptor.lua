2129bdb49e36f19cd82e34b4562754d09ec1dfe2
Renuka Fernando <renukapiyumal@gmail.com>
2021-10-11 21:57:04 +0530

    Update lib interceptor.lua to handle includes in the request Define which should include in the request payload of the interceptor

--- router/src/main/resources/interceptor/lib/interceptor.lua
+++ router/src/main/resources/interceptor/lib/interceptor.lua
@@ -1,26 +1,35 @@
+--- interceptor module
+-- @module interceptor
+local interceptor = {}
+
 local json = require 'home.wso2.interceptor.lib.json'
 -- json library: https://github.com/rxi/json.lua
 local base64 = require 'home.wso2.interceptor.lib.base64'
 -- base64 library: https://github.com/iskolbin/lbase64
 
-local interceptor = {}
 -- TODO: (renuka) check what to log as trace and debug
 
-local function send_http_call(handle, interceptor_request_body, cluster_name, resource_path, timeout)
-    local err, interceptor_response_body_str = handle:httpCall(
-        cluster_name,
+--- send an HTTP request to the interceptor
+-- @param handle request/response handler object
+-- @param interceptor_request_body a table of request body for the interceptor service
+-- @param intercept_service a table of connection details for the interceptor service
+-- @return a table of respose headers
+-- @return a table of response body
+local function send_http_call(handle, interceptor_request_body, intercept_service)
+    local headers, interceptor_response_body_str = handle:httpCall(
+        intercept_service["cluster_name"],
         {
             [":method"] = "POST",
-            [":path"] = resource_path,
+            [":path"] = intercept_service["resource_path"],
             [":authority"] = "cc-interceptor",
             ["content-type"] = "application/json",
             ["accept"] = "application/json",
         },
         json.encode(interceptor_request_body),
-        timeout,
+        intercept_service["timeout"],
         false -- async false, wait for response from interceptor service
     )
-    return err, interceptor_response_body_str
+    return headers, interceptor_response_body_str
 end
 
 local function modify_headers(interceptor_response_body, handle)
@@ -54,7 +63,23 @@ end
 local shared_request_headers
 local shared_request_body
 
-function interceptor.handle_request_interceptor(request_handle, cluster_name, resource_path, timeout)
+--- interceptor handler
+-- @param request_handle request handle object
+-- @param intercept_service a table of connection details for the interceptor service
+-- @param req_includes a table which describes what should be included in the request to the iterceptor service in the request flow
+-- @param resp_includes a table which describes what should be included in the request to the iterceptor service in the response flow
+function interceptor.handle_request_interceptor(request_handle, intercept_service, req_includes, resp_includes)
+    request_handle:logInfo("read metadata2")
+    local proto = request_handle:streamInfo():protocol()
+    request_handle:logInfo("PROTOCOL: " .. proto)
+    request_handle:logInfo("downstreamLocalAddress: " .. request_handle:streamInfo():downstreamLocalAddress())
+    request_handle:logInfo("downstreamDirectRemoteAddress: " .. request_handle:streamInfo():downstreamDirectRemoteAddress())
+    request_handle:logInfo("requestedServerName: " .. request_handle:streamInfo():requestedServerName())
+    local meta = request_handle:streamInfo():dynamicMetadata():get("envoy.filters.http.router")
+    for key, value in pairs(meta) do
+        request_handle:logInfo("key: " .. key .. " -> value: " .. value)
+    end
+
     -- read headers and the payload
     local request_headers = request_handle:headers()
     local request_id = request_headers:get("x-request-id")
@@ -74,14 +99,14 @@ function interceptor.handle_request_interceptor(request_handle, cluster_name, re
     }
 
     -- TODO: (renuka) handle errors
-    local _, interceptor_response_body_str = send_http_call(request_handle, interceptor_request_body, cluster_name, resource_path, timeout)
+    local _, interceptor_response_body_str = send_http_call(request_handle, interceptor_request_body, intercept_service)
     local interceptor_response_body = json.decode(interceptor_response_body_str)
 
     modify_body(interceptor_response_body, request_handle, request_id)
     modify_headers(interceptor_response_body, request_handle)
 end
 
-function interceptor.handle_response_interceptor(response_handle, cluster_name, resource_path, timeout)
+function interceptor.handle_response_interceptor(response_handle, intercept_service)
     -- read headers and the payload
     local request_headers = response_handle:headers()
     local request_id = request_headers:get("x-request-id")
@@ -100,7 +125,7 @@ function interceptor.handle_response_interceptor(response_handle, cluster_name, 
     }
 
     -- TODO: (renuka) handle errors
-    local _, interceptor_response_body_str = send_http_call(response_handle, interceptor_request_body, cluster_name, resource_path, timeout)
+    local _, interceptor_response_body_str = send_http_call(response_handle, interceptor_request_body, intercept_service)
     local interceptor_response_body = json.decode(interceptor_response_body_str)
 
     modify_body(interceptor_response_body, response_handle, request_id)
