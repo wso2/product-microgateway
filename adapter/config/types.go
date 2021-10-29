ccf0d7f6b0531a1b625d16266a7e655e24190500
User <viraj.salaka@gmail.com>
2021-10-29 10:14:16 +0530

    refactor configurations with support for old properties

--- adapter/config/types.go
+++ adapter/config/types.go
@@ -59,6 +59,11 @@ func NewReceiver() chan string {
 	return C
 }
 
+const (
+	//UnassignedAsDeprecated is used by the configurations which are deprecated.
+	UnassignedAsDeprecated string = "unassigned-as-deprecated"
+)
+
 // Config represents the adapter configuration.
 // It is created directly from the configuration toml file.
 // Note :
@@ -156,8 +161,10 @@ type vhostMapping struct {
 }
 
 type consul struct {
-	// Enable whether consul service discovery should be enabled
+	// Deprecated: Use Enabled instead
 	Enable bool
+	// Enabled whether consul service discovery should be enabled
+	Enabled bool
 	// URL url of the consul client in format: http(s)://host:port
 	URL string
 	// PollInterval how frequently consul API should be polled to get updates (in seconds)
@@ -205,10 +212,10 @@ type upstreamTLS struct {
 }
 
 type upstreamHealth struct {
-	Timeout            int32 `toml:"timeout"`
-	Interval           int32 `toml:"interval"`
-	UnhealthyThreshold int32 `toml:"unhealthyThreshold"`
-	HealthyThreshold   int32 `toml:"healthyThreshold"`
+	Timeout            int32
+	Interval           int32
+	UnhealthyThreshold int32
+	HealthyThreshold   int32
 }
 
 type upstreamRetry struct {
@@ -263,16 +270,20 @@ type throttlingConfig struct {
 	EnableQueryParamConditions         bool
 	EnableJwtClaimConditions           bool
 	JmsConnectionInitialContextFactory string
-	JmsConnectionProviderURL           string `toml:"jmsConnectionProviderUrl"`
+	JmsConnectionProviderURL           string
+	// Deprecated: Use JmsConnectionProviderURL instead
+	JmsConnectionProviderURLDeprecated string `toml:"jmsConnectionProviderUrl"`
 	Publisher                          binaryPublisher
 }
 
 type binaryPublisher struct {
 	Username string
 	Password string
-	URLGroup []urlGroup `toml:"urlGroup"`
-	Pool     publisherPool
-	Agent    binaryAgent
+	URLGroup []urlGroup
+	// Deprecated: Use URLGroup instead
+	URLGroupDeprecated []urlGroup `toml:"urlGroup"`
+	Pool               publisherPool
+	Agent              binaryAgent
 }
 
 type urlGroup struct {
@@ -310,7 +321,9 @@ type binaryAgent struct {
 }
 
 type jwtGenerator struct {
+	// Deprecated: Use Enabled instead
 	Enable                bool
+	Enabled               bool
 	Encoding              string
 	ClaimDialect          string
 	ConvertDialect        bool
@@ -395,8 +408,10 @@ type APICtlUser struct {
 
 // ControlPlane struct contains configurations related to the API Manager
 type controlPlane struct {
-	Enabled                    bool
-	ServiceURL                 string `toml:"serviceUrl"`
+	Enabled    bool
+	ServiceURL string
+	// Deprecated: Use ServiceURL instead.
+	ServiceURLDeprecated       string `toml:"serviceUrl"`
 	Username                   string
 	Password                   string
 	SyncApisOnStartUp          bool
@@ -408,10 +423,14 @@ type controlPlane struct {
 }
 
 type globalAdapter struct {
-	Enabled           bool
-	ServiceURL        string `toml:"serviceUrl"`
-	LocalLabel        string
+	Enabled    bool
+	ServiceURL string
+	// Deprecated: Use ServiceURL instead.
+	ServiceURLDeprecated string `toml:"serviceUrl"`
+	LocalLabel           string
+	// Deprecated: Use OverrideHostName instead.
 	OverwriteHostName string
+	OverrideHostName  string
 	RetryInterval     time.Duration
 }
 
@@ -423,7 +442,9 @@ type brokerConnectionParameters struct {
 
 // Configuration for Enforcer admin rest api
 type restServer struct {
-	Enable bool
+	// Deprecated: Use Enabled Instead
+	Enable  bool
+	Enabled bool
 }
 
 // Enforcer admin credentials
