2edbcb117b0cc07aa700e2f44f6b5078fcb1f921
slahirucd7 <slahiruc007@gmail.com>
2022-02-18 19:52:36 +0530

    preperations for 1.1.0-m3 release

--- remove-containers.sh
+++ remove-containers.sh
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------------
 
 # Just to assist developers by removing all the containers.
-cc_version=1.1.0-m2
+cc_version=1.1.0-m3
 
 docker kill "$(docker ps -a | grep wso2/choreo-connect-enforcer:$cc_version | awk '{print $1}')"
 docker rm "$(docker ps -a | grep wso2/choreo-connect-enforcer:$cc_version | awk '{print $1}')"
