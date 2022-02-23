b3ea1d77f81964e51850be76ca573b1785334239
maamalilakshika@gmail.com <maamalilakshika@gmail.com>
2022-02-23 15:36:43 +0530

    dns refresh improve

--- adapter/config/types.go
+++ adapter/config/types.go
@@ -195,6 +195,7 @@ type envoyUpstream struct {
 	TLS      upstreamTLS
 	Timeouts upstreamTimeout
 	Health   upstreamHealth
+	DNS      upstreamDNS
 	Retry    upstreamRetry
 }
 
@@ -218,6 +219,10 @@ type upstreamHealth struct {
 	Interval           int32
 	UnhealthyThreshold int32
 	HealthyThreshold   int32
+}
+
+type upstreamDNS struct {
+	DNSRefreshRate int32
 }
 
 type upstreamRetry struct {
