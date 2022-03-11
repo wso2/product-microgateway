b8952c15921816233d93b0b19270890e40be86fe
shanakama <shanakama@wso2.com>
2022-03-11 10:42:38 +0530

    Add condition for filter policy events

--- adapter/internal/messaging/notification_listener.go
+++ adapter/internal/messaging/notification_listener.go
@@ -41,6 +41,7 @@ const (
 	applicationEventType        = "APPLICATION"
 	subscriptionEventType       = "SUBSCRIPTION"
 	scopeEvenType               = "SCOPE"
+	policyEventType             = "POLICY"
 	removeAPIFromGateway        = "REMOVE_API_FROM_GATEWAY"
 	deployAPIToGateway          = "DEPLOY_API_IN_GATEWAY"
 	applicationRegistration     = "APPLICATION_REGISTRATION_CREATE"
@@ -126,8 +127,10 @@ func processNotificationEvent(conf *config.Config, notification *msg.EventNotifi
 		handleApplicationEvents(decodedByte, eventType)
 	} else if strings.Contains(eventType, subscriptionEventType) {
 		handleSubscriptionEvents(decodedByte, eventType)
-	} else {
+	} else if strings.Contains(eventType, policyEventType) {
 		handlePolicyEvents(decodedByte, eventType)
+	} else {
+		// filter all other events including HEALTH_CHECK event
 	}
 	return nil
 }
