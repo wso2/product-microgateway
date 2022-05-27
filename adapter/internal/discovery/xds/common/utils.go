819883bef0563bbcb4135ec42df395fdb20c92a7
VirajSalaka <viraj.salaka@gmail.com>
2022-05-28 00:49:22 +0530

    resolve review comments: refactor

--- adapter/internal/discovery/xds/common/utils.go
+++ adapter/internal/discovery/xds/common/utils.go
@@ -43,11 +43,11 @@ type NodeQueue struct {
 // 3. If the array is at the maximum length and element is inside the array, the new element should be appended and the already
 // 		existing entry should be removed from the position.
 // Returns the modified array and true if the entry is a new addition.
-func CheckEntryAndSwapToEnd(array []string, nodeID string) (modifiedArray []string, isNewAddition bool) {
+func (nodeQueue *NodeQueue) checkEntryAndMoveToEnd(nodeID string) (isNewAddition bool) {
 	matchedIndex := -1
-	arraySize := len(array)
+	arraySize := len(nodeQueue.queue)
 	for index := arraySize - 1; index >= 0; index-- {
-		entry := array[index]
+		entry := nodeQueue.queue[index]
 		if entry == nodeID {
 			matchedIndex = index
 			break
@@ -55,17 +55,17 @@ func CheckEntryAndSwapToEnd(array []string, nodeID string) (modifiedArray []stri
 	}
 
 	if matchedIndex == nodeIDArrayMaxLength-1 {
-		return array, false
+		return false
 	} else if matchedIndex > 0 {
-		array = append(array[0:matchedIndex], array[matchedIndex+1:]...)
-		array = append(array, nodeID)
-		return array, false
+		nodeQueue.queue = append(nodeQueue.queue[0:matchedIndex], nodeQueue.queue[matchedIndex+1:]...)
+		nodeQueue.queue = append(nodeQueue.queue, nodeID)
+		return false
 	}
 	if arraySize >= nodeIDArrayMaxLength {
-		array = array[1:]
+		nodeQueue.queue = nodeQueue.queue[1:]
 	}
-	array = append(array, nodeID)
-	return array, true
+	nodeQueue.queue = append(nodeQueue.queue, nodeID)
+	return true
 }
 
 // GenerateNodeQueue creates an instance of nodeQueue with a mutex and a string array assigned.
@@ -77,12 +77,10 @@ func GenerateNodeQueue() *NodeQueue {
 }
 
 // IsNewNode returns true if the provided nodeID does not exist in the nodeQueue
-func IsNewNode(nodeQueueInstance *NodeQueue, nodeIdentifier string) bool {
-	nodeQueueInstance.lock.Lock()
-	defer nodeQueueInstance.lock.Unlock()
-	var isNewAddition bool
-	nodeQueueInstance.queue, isNewAddition = CheckEntryAndSwapToEnd(nodeQueueInstance.queue, nodeIdentifier)
-	return isNewAddition
+func (nodeQueue *NodeQueue) IsNewNode(nodeIdentifier string) bool {
+	nodeQueue.lock.Lock()
+	defer nodeQueue.lock.Unlock()
+	return nodeQueue.checkEntryAndMoveToEnd(nodeIdentifier)
 }
 
 // GetNodeIdentifier constructs the nodeIdentifier from discovery request's node property, label:<instanceIdentifierProperty>
