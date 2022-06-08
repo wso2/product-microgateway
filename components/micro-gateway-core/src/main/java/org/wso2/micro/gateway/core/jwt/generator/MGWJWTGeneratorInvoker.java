1a49767fdb46ff063d0ff6b5f8c97afbd16c9831
chashikajw <chashikajw007@gmail.com>
2022-06-08 18:17:43 +0530

    Prepare for 3.2.4 release

--- components/micro-gateway-core/src/main/java/org/wso2/micro/gateway/core/jwt/generator/MGWJWTGeneratorInvoker.java
+++ components/micro-gateway-core/src/main/java/org/wso2/micro/gateway/core/jwt/generator/MGWJWTGeneratorInvoker.java
@@ -106,7 +106,7 @@ public class MGWJWTGeneratorInvoker {
             if (claimList == null) {
                 return null;
             }
-            BPackage packageId = new BPackage("wso2", "gateway", "3.2.3");
+            BPackage packageId = new BPackage("wso2", "gateway", "3.2.4");
             BMap<String, Object> bMap = BValueCreator.createRecordValue(packageId, "RetrievedUserClaimsListDTO");
             bMap.put("count", claimList.size());
             BArray bArray = (BArray) bMap.get("list");
