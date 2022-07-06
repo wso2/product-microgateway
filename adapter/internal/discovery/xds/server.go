3e20efbecf70263abfea0925b7102515278b7449
Viraj Gamage <viraj.salaka@gmail.com>
2022-07-06 10:07:10 +0530

    revert changing the lifecycle status to CREATED/PUBLISHED

--- adapter/internal/discovery/xds/server.go
+++ adapter/internal/discovery/xds/server.go
@@ -121,7 +121,7 @@ const (
 	prototypedAPI        string = "PROTOTYPED"
 	apiKeyFieldSeparator string = ":"
 	blockedStatus        string = "BLOCKED"
-	publishedStatus      string = "PUBLISHED"
+	nonBlockedStatus      string = "CREATED/PUBLISHED"
 )
 
 // IDHash uses ID field as the node hash.
@@ -1308,5 +1308,5 @@ func updateLCStateOfMgwSwagger(mgwSwagger *model.MgwSwagger) {
 		mgwSwagger.LifeCycleState = blockedStatus
 		return
 	}
-	mgwSwagger.LifeCycleState = publishedStatus
+	mgwSwagger.LifeCycleState = nonBlockedStatus
 }
