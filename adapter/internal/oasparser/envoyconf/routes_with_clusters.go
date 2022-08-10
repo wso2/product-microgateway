1e5df1ff6b89533e2ec84454324a4705364a8e49
Lahiru De Silva <lahirude@wso2.com>
2022-08-10 12:37:29 +0530

    RequestHeadersToRemove in Route configuration instead of Route

--- adapter/internal/oasparser/envoyconf/routes_with_clusters.go
+++ adapter/internal/oasparser/envoyconf/routes_with_clusters.go
@@ -678,13 +678,11 @@ func createRoute(params *routeCreateParams) *routev3.Route {
 
 	logger.LoggerOasparser.Debug("creating a route....")
 	var (
-		router                  routev3.Route
-		action                  *routev3.Route_Route
-		match                   *routev3.RouteMatch
-		decorator               *routev3.Decorator
-		resourcePath            string
-		requestHeadersToRemove  []string
-		responseHeadersToRemove []string
+		router       routev3.Route
+		action       *routev3.Route_Route
+		match        *routev3.RouteMatch
+		decorator    *routev3.Decorator
+		resourcePath string
 	)
 	basePath := getFilteredBasePath(xWso2Basepath, endpointBasepath)
 
@@ -887,9 +885,6 @@ func createRoute(params *routeCreateParams) *routev3.Route {
 	if corsPolicy != nil {
 		action.Route.Cors = corsPolicy
 	}
-	requestHeadersToRemove = append(requestHeadersToRemove, clusterHeaderName)
-	// remove the 'x-envoy-upstream-service-time' from the response.
-	responseHeadersToRemove = append(responseHeadersToRemove, upstreamServiceTimeHeader)
 
 	logger.LoggerOasparser.Debug("adding route ", resourcePath)
 	router = routev3.Route{
@@ -902,8 +897,6 @@ func createRoute(params *routeCreateParams) *routev3.Route {
 			wellknown.HTTPExternalAuthorization: extAuthzFilter,
 			wellknown.Lua:                       luaFilter,
 		},
-		RequestHeadersToRemove:  requestHeadersToRemove,
-		ResponseHeadersToRemove: responseHeadersToRemove,
 	}
 	return &router
 }
