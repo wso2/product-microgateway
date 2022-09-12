9ff92f256d7ebdafb6cd11937c6f22f40fdf480a
Amila Senadheera <amila.15@cse.mrt.ac.lk>
2022-09-12 11:08:19 +0530

    license added, system property name updated

--- enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/jmx/JMXUtils.java
+++ enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/jmx/JMXUtils.java
@@ -21,7 +21,7 @@ package org.wso2.choreo.connect.enforcer.jmx;
  */
 public class JMXUtils {
 
-    private static final String CHOREO_CONNECT_JMX_METRICS_ENABLE = "choreo.connect.jmx.metrics.enable";
+    private static final String CHOREO_CONNECT_JMX_METRICS_ENABLE = "choreo.connect.jmx.metrics.enabled";
 
     /**
      * Returns true if jmx metrics enabled as a system property, otherwise false.
