533ecf79928be9bccc84a9a6710540984bf2719c
slahirucd7 <slahiruc007@gmail.com>
2022-09-14 09:03:18 +0530

    dropins dir creation

--- dev-scripts/code-coverage/prepare-files-before-int-tests.sh
+++ dev-scripts/code-coverage/prepare-files-before-int-tests.sh
@@ -17,6 +17,7 @@
 
 # Sets the Choreo Connect version
 echo "Preparing code coverage files before integration tests..."
+mkdir ../../resources/enforcer/dropins/
 cp ../../enforcer-parent/enforcer/target/coverage-aggregate-reports/aggregate.exec ../../resources/enforcer/dropins/
 chmod 777 ../../resources/enforcer/dropins/aggregate.exec
 #JAVA_OPTS=-javaagent:/home/wso2/lib/org.jacoco.agent-0.8.8-runtime.jar=destfile=/home/wso2/lib/dropins/aggregate.exec,append=true
