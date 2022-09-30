97ed57a3ebc671a08887cc3358e7d0732a90f7bd
Renuka Fernando <renukapiyumal@gmail.com>
2022-09-30 17:48:12 +0530

    Fix new line issue when certs are created in a Windows platform

--- enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/util/JWTUtils.java
+++ enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/util/JWTUtils.java
@@ -161,11 +161,11 @@ public class JWTUtils {
             String strKeyPEM;
             Path keyPath = Paths.get(filePath);
             String key = Files.readString(keyPath, Charset.defaultCharset());
-            String lineSeparator = System.lineSeparator();
-            
+
             strKeyPEM = key
                     .replace(Constants.BEGINING_OF_PRIVATE_KEY, "")
-                    .replaceAll(lineSeparator, "")
+                    .replaceAll("\r\n", "") // for certs created in a Windows platform
+                    .replaceAll("\n", "") // for certs created in a Unix platform
                     .replace(Constants.END_OF_PRIVATE_KEY, "");
 
             byte[] encoded = Base64.getDecoder().decode(strKeyPEM);
