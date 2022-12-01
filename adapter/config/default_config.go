92b8d2447418bc13c3243d25f7a2b1ec2889c635
Subhashinie Koshalya <subhashinie.ksw@gmail.com>
2022-12-01 14:16:04 +0530

    Update sandbox vhost default value. Add newlines.

--- adapter/config/default_config.go
+++ adapter/config/default_config.go
@@ -58,7 +58,7 @@ var defaultConfig = &Config{
 			Location: "/home/wso2/security/truststore",
 		},
 		ArtifactsDirectory: "/home/wso2/artifacts",
-		SandboxVhost:       "vhost",
+		SandboxVhost:       "sandbox.host",
 	},
 	Envoy: envoy{
 		ListenerHost:                     "0.0.0.0",
