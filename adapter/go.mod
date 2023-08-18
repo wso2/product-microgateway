03d0ff28b60e6eb0f359d1bf6a2ce22c2726db85
Avishka-Shamendra <avishka199849@gmail.com>
2023-08-18 15:20:00 +0530

    Expose golang cpu,memory related metrics with gopsutil

--- adapter/go.mod
+++ adapter/go.mod
@@ -25,9 +25,10 @@ require (
 	github.com/mitchellh/mapstructure v1.3.3
 	github.com/pelletier/go-toml v1.8.1
 	github.com/prometheus/client_golang v1.16.0
+	github.com/shirou/gopsutil/v3 v3.23.7
 	github.com/sirupsen/logrus v1.7.0
 	github.com/streadway/amqp v1.0.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.4
 	github.com/vektah/gqlparser/v2 v2.5.1
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.7.0
@@ -58,6 +59,7 @@ require (
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/analysis v0.19.10 // indirect
 	github.com/go-openapi/jsonpointer v0.19.3 // indirect
 	github.com/go-openapi/jsonreference v0.19.3 // indirect
@@ -70,20 +72,26 @@ require (
 	github.com/lestrrat-go/httpcc v1.0.0 // indirect
 	github.com/lestrrat-go/iter v1.0.0 // indirect
 	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mailru/easyjson v0.7.1 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.mongodb.org/mongo-driver v1.7.5 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- adapter/pkg/metrics/metrics.go
+++ adapter/pkg/metrics/metrics.go
@@ -2,16 +2,98 @@ package metrics
 
 import (
 	"net/http"
+	"os"
+	"runtime"
 	"strconv"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/load"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 // PrometheusMetricType prometheus metric type
 const PrometheusMetricType = "prometheus"
 
+var hostInfo = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "host_info",
+	Help: "Host Info",
+}, []string{"os"})
+
+var availableCPUs = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "os_available_cpus",
+	Help: "Number of available CPUs",
+})
+
+var freePhysicalMemory = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "os_free_physical_memory",
+	Help: "Amount of free physical memory in bytes",
+})
+
+var systemCPULoad = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "os_system_cpu_load",
+	Help: "System-wide CPU usage as a percentage",
+})
+
+var processCPULoad = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "os_process_cpu_load_percentage",
+	Help: "CPU usage of the current process as a percentage",
+})
+
+var loadAvg = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "os_system_load_average",
+	Help: "Current load of CPU in the host system for the last {x} minutes",
+}, []string{"duration"})
+
+func init() {
+	prometheus.MustRegister(hostInfo, availableCPUs, freePhysicalMemory, systemCPULoad, processCPULoad, loadAvg)
+}
+
+// recordMetrics record custom golang metrics
+func recordMetrics() {
+	for {
+		host, err := host.Info()
+		hostInfo.WithLabelValues(host.OS).Set(1)
+
+		availableCPUs.Set(float64(runtime.NumCPU()))
+
+		v, err := mem.VirtualMemory()
+		if err == nil {
+			freePhysicalMemory.Set(float64(v.Free))
+		}
+
+		percentages, err := cpu.Percent(0, false)
+		if err == nil && len(percentages) > 0 {
+			systemCPULoad.Set(percentages[0])
+		}
+
+		p, err := process.NewProcess(int32(os.Getpid()))
+		if err == nil {
+			percent, _ := p.CPUPercent()
+			processCPULoad.Set(percent)
+		}
+
+		avg, err := load.Avg()
+		if err != nil {
+			return
+		}
+		loadAvg.WithLabelValues("1m").Set(avg.Load1)
+		loadAvg.WithLabelValues("5m").Set(avg.Load5)
+		loadAvg.WithLabelValues("15m").Set(avg.Load15)
+
+		// Sleep 5s before the next measurement
+		time.Sleep(5 * time.Second)
+	}
+
+}
+
 // StartPrometheusMetricsServer initializes and starts the metrics server to expose metrics to prometheus.
 func StartPrometheusMetricsServer(port int32) {
+	go recordMetrics()
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":"+strconv.FormatInt(int64(port), 10), nil)
 }
