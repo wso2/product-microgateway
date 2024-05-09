f4314863be9c123642bf2a1eebd44d60b062ec7f
SLahiruC <40932779+slahirucd7@users.noreply.github.com>
2024-05-09 11:41:32 +0530

    changes for review suggestions

--- enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/security/jwt/JWTAuthenticator.java
+++ enforcer-parent/enforcer/src/main/java/org/wso2/choreo/connect/enforcer/security/jwt/JWTAuthenticator.java
@@ -348,9 +348,12 @@ public class JWTAuthenticator implements Authenticator {
                             requestContext.addMetadataToMap("ratelimit:organization",
                                     APIConstants.SUPER_TENANT_DOMAIN_NAME);
                         }
-                        log.debug("Organization ID: " + requestContext.getMetadataMap().get("ratelimit:organization")
-                                + ", SubscriptionId: " + subscriptionId + ", SubscriptionPolicy: " + subPolicyName
-                                + " will be evaluated for subscription rate-limiting");
+                        if (log.isDebugEnabled()) {
+                            log.debug("Organization ID: " +
+                                    requestContext.getMetadataMap().get("ratelimit:organization")
+                                    + ", SubscriptionId: " + subscriptionId + ", SubscriptionPolicy: " + subPolicyName
+                                    + " will be evaluated for subscription rate-limiting");
+                        }
                     }
                     return authenticationContext;
                 } else {
