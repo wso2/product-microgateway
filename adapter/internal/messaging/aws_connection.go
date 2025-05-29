fc6a2883a142c306f882f912d0dae0b2607e36a3
SLahiruC <40932779+slahirucd7@users.noreply.github.com>
2025-05-29 08:32:40 +0530

    code refactoring considering the aws active mq implementation

--- adapter/internal/messaging/aws_connection.go
+++ adapter/internal/messaging/aws_connection.go
@@ -20,30 +20,32 @@ package messaging
 import (
 	"context"
 
-	"github.com/Azure/go-amqp"
 	"github.com/wso2/product-microgateway/adapter/config"
 	logger "github.com/wso2/product-microgateway/adapter/internal/loggers"
 	"github.com/wso2/product-microgateway/adapter/pkg/health"
+	msg "github.com/wso2/product-microgateway/adapter/pkg/messaging"
 )
 
 var awsActiveMqtopicNames = []string{tokenRevocation, notification, stepQuotaThreshold, stepQuotaReset}
 
 // InitiateAndProcessAWSActiveMQEvents initiates AWS ActiveMQ connection and starts consuming messages from the specified topics.
-func InitiateAndProcessAWSActiveMQEvents(config1 *config.Config) {
-	conf, _ := config.ReadConfigs()
+func InitiateAndProcessAWSActiveMQEvents(conf *config.Config) {
 	for _, topic := range awsActiveMqtopicNames {
 		logger.LoggerMgw.Debugf("Initiating AWS ActiveMQ connection for topic: %s", topic)
 		connectionString := conf.ControlPlane.BrokerConnectionParameters.EventListeningEndpoints[0]
+		username := conf.ControlPlane.BrokerConnectionParameters.ActiveMqUsername
+		password := conf.ControlPlane.BrokerConnectionParameters.ActiveMqPassword
 		ctx := context.Background()
 
-		con, err := initAwsActiveMqConnection(ctx, connectionString)
+		con, err := msg.InitAwsActiveMqConnection(ctx, connectionString, username, password)
 		if err != nil {
 			logger.LoggerMgw.Errorf("Failed to connect to AWS ActiveMQ broker for topic: %s error:%s", topic, err.Error())
 			return
 		}
 		logger.LoggerMgw.Debugf("AWS ActiveMQ connection established for topic: %s", topic)
+		maximumallowedUnacknowledgedMessages := conf.ControlPlane.BrokerConnectionParameters.MaximumAllowedUnacknowledgedMessages
 
-		receiver, receiverErr := initAwsActiveMqConnectionAndValidate(connectionString, topic, con)
+		receiver, receiverErr := msg.InitAwsActiveMqReceiverAndValidate(connectionString, topic, con, maximumallowedUnacknowledgedMessages)
 		if receiverErr != nil {
 			defer con.Close()
 			logger.LoggerMgw.Errorf("Failed to a create receiver for topic: %s error:%s", topic, receiverErr.Error())
@@ -52,43 +54,4 @@ func InitiateAndProcessAWSActiveMQEvents(config1 *config.Config) {
 		startActiveMQTopicConsumer(topic, receiver)
 	}
 	health.SetControlPlaneBrokerStatus(true)
-}
-
-func initAwsActiveMqConnectionAndValidate(connectionString string, topicName string, con *amqp.Conn) (*amqp.Receiver, error) {
-	conf, _ := config.ReadConfigs()
-	session, sessionErr := con.NewSession(context.Background(), nil)
-	if sessionErr != nil {
-		logger.LoggerMgw.Errorf("Failed to create session for topic: %s error:%s", topicName, sessionErr.Error())
-		return nil, sessionErr
-	}
-	receiver, receiverErr := session.NewReceiver(
-		context.Background(),
-		"topic://"+topicName,
-		&amqp.ReceiverOptions{
-			Credit: int32(conf.ControlPlane.BrokerConnectionParameters.MaximumAllowedUnacknowledgedMessages),
-		},
-	)
-	if receiverErr != nil {
-		logger.LoggerMgw.Errorf("Failed to create receiver for topic: %s error:%s", topicName, receiverErr.Error())
-		return nil, receiverErr
-	}
-	return receiver, nil
-}
-
-func initAwsActiveMqConnection(ctx context.Context, connectionString string) (*amqp.Conn, error) {
-	conf, _ := config.ReadConfigs()
-	con, err := amqp.Dial(
-		ctx,
-		connectionString,
-		&amqp.ConnOptions{
-			SASLType: amqp.SASLTypePlain(
-				conf.ControlPlane.BrokerConnectionParameters.ActiveMqUsername,
-				conf.ControlPlane.BrokerConnectionParameters.ActiveMqPassword),
-		},
-	)
-	if err != nil {
-		logger.LoggerMgw.Infof("Failed to connect to AWS ActiveMQ broker: %s" + err.Error())
-		return nil, err
-	}
-	return con, nil
 }
---
+++ adapter/pkg/messaging/aws_active_mq_connection.go
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package messaging
+
+import (
+	"context"
+
+	"github.com/Azure/go-amqp"
+	logger "github.com/wso2/product-microgateway/adapter/internal/loggers"
+)
+
+// InitAwsActiveMqConnection initializes a connection to the AWS ActiveMQ broker
+func InitAwsActiveMqConnection(ctx context.Context, connectionString string, userName string, password string) (*amqp.Conn, error) {
+	con, err := amqp.Dial(
+		ctx,
+		connectionString,
+		&amqp.ConnOptions{
+			SASLType: amqp.SASLTypePlain(
+				userName,
+				password),
+		},
+	)
+	if err != nil {
+		logger.LoggerMgw.Infof("Failed to connect to AWS ActiveMQ broker: %s" + err.Error())
+		return nil, err
+	}
+	return con, nil
+}
+
+// InitAwsActiveMqReceiverAndValidate initializes a receiver for the specified topic on the AWS ActiveMQ broker
+func InitAwsActiveMqReceiverAndValidate(connectionString string, topicName string, con *amqp.Conn, maximumallowedUnacknowledgedMessages int) (*amqp.Receiver, error) {
+	session, sessionErr := con.NewSession(context.Background(), nil)
+	if sessionErr != nil {
+		logger.LoggerMgw.Errorf("Failed to create session for topic: %s error:%s", topicName, sessionErr.Error())
+		return nil, sessionErr
+	}
+	receiver, receiverErr := session.NewReceiver(
+		context.Background(),
+		"topic://"+topicName,
+		&amqp.ReceiverOptions{
+			Credit: int32(maximumallowedUnacknowledgedMessages),
+		},
+	)
+	if receiverErr != nil {
+		logger.LoggerMgw.Errorf("Failed to create receiver for topic: %s error:%s", topicName, receiverErr.Error())
+		return nil, receiverErr
+	}
+	return receiver, nil
+}
